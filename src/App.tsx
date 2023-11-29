f9f10c35f8ae26f574b2f9aaa3753ca77d873f67
Julio Alexandre <julioalexandre499@gmail.com>
2023-11-28 23:34:52 -0300

    project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import Produtos from './pages/Produtos/Produtos';
+import NotFound from './pages/NotFound/NotFound';
+import Layout from './Components/Layout/Layout';
+import Checkout from './pages/Checkout/Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={ <Layout /> }>
+        <Route index element={ <Home /> } />
+        <Route path="/carrinho" element={ <Cart /> } />
+        <Route path="/produtos/:id" element={ <Produtos /> } />
+        <Route path="/checkout" element={ <Checkout /> } />
+      </Route>
+      <Route path="*" element={ <NotFound /> } />
+    </Routes>
   );
 }
 
